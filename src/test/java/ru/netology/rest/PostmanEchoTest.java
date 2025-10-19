f1f1c10cab1cea7b0553ef035836dffce64b85b9
Bronks <Nazim.said@yandex.ru>
2025-10-19 16:29:14 +0300

    Build well

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -14,7 +14,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then().log().all()
                 .statusCode(200)
-                .body("data", org.hamcrest.Matchers.equalTo("Hi"));
+                .body("data", org.hamcrest.Matchers.equalTo("So difficult"));
 
     }
 
