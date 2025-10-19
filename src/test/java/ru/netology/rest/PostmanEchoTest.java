1f75943cc92446e60b119437c8829b599369a49d
Bronks <Nazim.said@yandex.ru>
2025-10-19 16:13:33 +0300

    first commit

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,21 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PostmanEchoTest {
+    @Test
+    public void shouldReturnSendData(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hi")
+                .when().log().all()
+                .post("/post")
+                .then().log().all()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo("Hi"));
+
+    }
+
+}
